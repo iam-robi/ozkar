0fbaea5eda1d16ec08f080d11374ea14854095bf
robi <robin.sarfati@realconsultingdata.com>
2023-10-29 22:31:44 +0100

    mutation verifier

--- packages/contracts/src/GeneProof.ts
+++ packages/contracts/src/GeneProof.ts
@@ -26,16 +26,6 @@ export let dnaSeq = new ZKSeq(dnaSample);
 let dnaFieldArray = dnaSeq.toFieldArray();
 export class DnaFieldArray extends DynamicArray(Field, dnaLength) {}
 
-/**
- * Basic Example
- * See https://docs.minaprotocol.com/zkapps for more info.
- *
- * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
- * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
- *
- * This file is safe to delete and replace with your own contract.
- */
-
 export class GeneProof extends SmartContract {
   @state(Field) geneHash = State<Field>();
   @state(Field) dnaSeqSize = State<Field>();
@@ -94,6 +84,66 @@ export class GeneProof extends SmartContract {
       Provable.log('geneFound', geneFound);
 
       geneFound.assertGreaterThan(0);
+    }
+  }
+
+  //TODO: prove presence of gene variant with parameter variant limit , variant range (ie: presence of a variant in a specific range)
+  @method verifyMutation(dnaSeq: DnaFieldArray, geneSeq: GeneFieldArray) {
+    let dnaSeqSize = dnaSeq.maxLength();
+    let geneSeqSize = geneSeq.maxLength();
+    let loopSize = dnaSeqSize - geneSeqSize + 1;
+    let variantFound = Field(0);
+    Field(loopSize).assertGreaterThan(0);
+    Provable.log(loopSize);
+
+    //for now we only look for SNPs
+    let variantMaxSize = Field(1);
+
+    for (let i = 0; i < loopSize; i++) {
+      let base: Field = dnaSeq.get(Field(i));
+
+      let matchCount: Field = Field(0);
+      let variantCount: Field = Field(0);
+      for (let j = 0; j < geneSeqSize; j++) {
+        let geneBase: Field = geneSeq.get(Field(j));
+        let index: Field = Field(i + j);
+        let dnaBase: Field = dnaSeq.get(index);
+
+        matchCount = Provable.if(
+          dnaBase.equals(geneBase),
+          matchCount.add(1),
+          matchCount
+        );
+
+        //TODO: if we do fixed size array we will have to account for trailing Fields (value of 0?)
+        variantCount = Provable.if(
+          dnaBase.equals(geneBase),
+          variantCount,
+          variantCount.add(1)
+        );
+
+        Provable.log(
+          i + j,
+          'dnaBase.equals(geneBase)',
+          dnaBase.equals(geneBase),
+          'matchCount',
+          matchCount
+        );
+        //TODO: stop execution at first unmatched to avoid unecesseraly looping
+      }
+
+      //TODO: option for proximity score, gee
+      variantFound = Provable.if(
+        matchCount.greaterThanOrEqual(
+          Field(geneSeq.maxLength()).sub(variantMaxSize)
+        ) && variantCount.greaterThanOrEqual(Field(0)),
+        variantCount.add(1),
+        variantCount.add(0)
+      );
+
+      Provable.log('variantFound', variantFound);
+
+      variantFound.assertGreaterThan(0);
     }
   }
 }
