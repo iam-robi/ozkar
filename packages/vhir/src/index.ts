f0d18e86af58736472a520129eb5d0d8ae3f5f30
robi <robins07@me.com>
2023-12-06 23:55:54 +0100

    :alien: Update export for ozkarchain

--- packages/vhir/src/index.ts
+++ packages/vhir/src/index.ts
@@ -1,5 +1,2 @@
-// import { BruteForceVerifier } from './sequence_verifiers/BruteForceVerifier';
-// import { SegmentVerifier } from './sequence_verifiers/SegmentVerifier';
-// import { ZKSeq } from './lib/consent';
-// import { DynamicArray } from './lib/dynamicArray';
-// export { BruteForceVerifier, SegmentVerifier, ZKSeq, DynamicArray };
+export { ZKConsent } from './resource/consent';
+export { DocumentReference } from './resource/document';
--- packages/vhir/src/resource/consent.ts
+++ packages/vhir/src/resource/consent.ts
@@ -37,7 +37,7 @@ interface ZKConsentInitArgs {
 }
 // https://www.hl7.org/fhir/consent.html
 //Consent management - particularly privacy consent - is complicated by the fact that consent to share is often itself necessary to protect. The need to protect the privacy of the privacy statement itself competes with the execution of the consent statement. For this reason, it is common to deal with 'consent statements' that are only partial representations of the full consent statement that the patient provided.
-export class ZKConsent extends Struct({
+export class Consent extends Struct({
   resourceType: CircuitString,
   sourceReference: CircuitString,
   //The entity responsible for granting the rights listed in a Consent Directive.
